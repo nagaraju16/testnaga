c0165747dc061adf3c0bbbb6d0c25a4c718ea73f
Ubuntu <ubuntu@ip-172-31-3-11.us-east-2.compute.internal>
2019-09-16 07:17:35 +0000

    initail commit

---
+++ calc.py
@@ -0,0 +1,18 @@
+dd(x, y):  
+       """This function adds two numbers"" 
+          return x + y 
+          def subtract(x, y): 
+             """This function subtracts two numbers""" 
+                return x - y 
+                def multiply(x, y): 
+                   """This function multiplies two numbers""" 
+                      return x * y 
+                      def divide(x, y): 
+                         """This function divides two numbers"""  
+                            return x / y  
+                            # take input from the user  
+                            print("Select operation.")  
+                            print("1.Add")  
+                            print("2.Subtract")  
+                            print("3.Multiply")  
+                            print("4.Divide")  
